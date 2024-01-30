d0276e845bf2637cfb76a0c4c66fc0d4ff897187
vanay <vanayfefilov777@gmail.com>
2024-01-30 17:51:51 +0100

    init

--- src/services/openService.ts
+++ src/services/openService.ts
@@ -6,12 +6,12 @@ const fd=fs.promises
 export class OpenService {
    static async getNotification(date,ip,id){
        console.log('date:',date);
+       console.log('ip in services:',ip);
        const notification= await NotificationModel.findOne({ip});
        if (notification)    throw ApiError.NotFound("You can't get a notifications");
-/*
        const filePath = path.join(__dirname, '..', 'services', 'url.txt');
-*/
-       const data = await fd.readFile('src/services/url.txt', "utf8");
+       console.log('filePath:',filePath);
+       const data = await fd.readFile(filePath, "utf8");
        const [link,date2]=data.split(";")
        console.log(link,date2)
        if (date2===date){
