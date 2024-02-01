3f33b344710ad143952d73f961e1bee717d66fd2
Ivan <82017106+vanaf25@users.noreply.github.com>
2024-02-01 17:54:49 +0100

    Update openService.ts

--- src/services/openService.ts
+++ src/services/openService.ts
@@ -5,11 +5,15 @@ const fd=fs.promises
 export class OpenService {
    static async getNotification(date,ip,id,param){
        console.log('date from request:',date);
+       console.log('typeof 1:', typeof date)
+       console.log('length 1:',date.length)
        const notification= await NotificationModel.findOne({ip});
        if (notification)    throw ApiError.NotFound("You can't get a notifications");
        const data = await fd.readFile('dist/services/url.txt', "utf8");
        const [link,date2]=data.split(";")
-       console.log('date from file: ',date2);
+       console.log('date from file:',date2);
+       console.log('typeof 1:', typeof date2);
+       console.log('length 1:',date2.length);
        console.log(`${date2}===${date}: `,date2===date);
        if (date2===date){
            console.log('id:',id);
