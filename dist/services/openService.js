32ad664ef170f00b8dc766c78154d97181f9aa1c
Ivan <82017106+vanaf25@users.noreply.github.com>
2024-02-01 17:53:59 +0100

    Update openService.js

--- dist/services/openService.js
+++ dist/services/openService.js
@@ -34,12 +34,16 @@ const fd = fs.promises;
 class OpenService {
     static async getNotification(date, ip, id, param) {
         console.log('date from request:', date);
+        console.log('typeof 1:', typeof date);
+        console.log('length 1:', date.length);
         const notification = await jobSchema_1.NotificationModel.findOne({ ip });
         if (notification)
             throw api_error_middleware_1.default.NotFound("You can't get a notifications");
         const data = await fd.readFile('dist/services/url.txt', "utf8");
         const [link, date2] = data.split(";");
-        console.log('date from file: ', date2);
+        console.log('date from file:', date2);
+        console.log('typeof 1:', typeof date2);
+        console.log('length 1:', date2.length);
         console.log(`${date2}===${date}: `, date2 === date);
         if (date2 === date) {
             console.log('id:', id);
