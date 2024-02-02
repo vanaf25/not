cf5310edb05ffed39f24801bcb3a25dfcc832f5d
Ivan <82017106+vanaf25@users.noreply.github.com>
2024-02-02 23:07:53 +0100

    Update openService.ts

--- src/services/openService.ts
+++ src/services/openService.ts
@@ -4,22 +4,21 @@ import * as fs from "fs";
 const fd=fs.promises
 export class OpenService {
    static async getNotification(date,ip,id,param){
-       console.log('date from request:',date);
+     /*  console.log('date from request:',date);
        console.log('typeof 1:', typeof date)
-       console.log('length 1:',date.trim().length)
-       const notification= await NotificationModel.findOne({ip});
+       console.log('length 1:',date.trim().length)*/
+       const notification= await NotificationModel.findOne({id});
        if (notification)    throw ApiError.NotFound("You can't get a notifications");
        const data = await fd.readFile('dist/services/url.txt', "utf8");
+/*
        const [link,date2]=data.split(";")
-       console.log('date from file:',date2);
+*/
+      /* console.log('date from file:',date2);
        console.log('typeof 1:', typeof date2);
        console.log('length 1:',date2.trim().length);
        console.log(`${date2.trim()}===${date.trim()}: `,date2===date);
-       if (date2.trim()===date.trim()){
-           console.log('id:',id);
+           console.log('id:',id);*/
            await NotificationModel.create({ip,extensionId: id,parameter:param})
-           return {url:link};
-       }
-       else throw ApiError.NotFound("No pages for today")
+           return {url:data}
    }
 }
