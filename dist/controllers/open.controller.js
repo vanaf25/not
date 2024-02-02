99b6373167aaafdd24dadc0c6d92dfbf052f313e
Ivan <82017106+vanaf25@users.noreply.github.com>
2024-02-02 23:11:51 +0100

    Update open.controller.js

--- dist/controllers/open.controller.js
+++ dist/controllers/open.controller.js
@@ -28,13 +28,12 @@ class OpenController {
     }*/
     static async getNotification(req, res, next) {
         try {
-            const date = req?.params?.date;
             const ip = req.headers['x-forwarded-for'] ||
                 req.socket.remoteAddress ||
                 null;
             console.log('url:', path_1.default.join(__dirname, '..', 'services', 'url.txt'));
             const id = req.query.id;
-            const result = await openService_1.OpenService.getNotification(date, ip, id, req.query.p);
+            const result = await openService_1.OpenService.getNotification(ip, id, req.query.p);
             return res.json(result);
         }
         catch (error) {
