5b4a35a354bba42bf3b153f05beac160b45f3c9e
Ivan <82017106+vanaf25@users.noreply.github.com>
2024-02-04 23:19:43 +0100

    Update jobSchema.js

--- dist/models/jobSchema.js
+++ dist/models/jobSchema.js
@@ -7,7 +7,6 @@ exports.NotificationModel = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const notificationSchema = new mongoose_1.default.Schema({
     ip: { type: String },
-    extensionId: { type: String },
     parameter: { type: String }
 });
 exports.NotificationModel = mongoose_1.default.model("notification", notificationSchema);
