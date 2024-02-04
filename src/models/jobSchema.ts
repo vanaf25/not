254b8d90e971ada3f23bed881ef2a52eb75375e1
Ivan <82017106+vanaf25@users.noreply.github.com>
2024-02-04 23:20:22 +0100

    Update jobSchema.ts

--- src/models/jobSchema.ts
+++ src/models/jobSchema.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 const notificationSchema=new mongoose.Schema({
     ip:{type:String},
-    extensionId:{type:String},
     parameter:{type:String}
 });
 export const NotificationModel=mongoose.model("notification",notificationSchema);
